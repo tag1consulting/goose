e662273e81e37c8fe8fb2f3d9cee386028de88af
Jeremy Andrews <jeremy@tag1consulting.com>
2020-03-03 13:31:41 +0100

    parse run_time string

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 extern crate structopt;
 
+// @TODO: load this as a dynamic library
 mod goosefile;
+mod util;
 
+use std::ffi::OsStr;
 use std::path::PathBuf;
 
 use structopt::StructOpt;
@@ -10,12 +13,12 @@ use structopt::StructOpt;
 #[structopt(name = "client")]
 pub struct Configuration {
     /// Host to load test in the following format: http://10.21.32.33
-    #[structopt(short = "H", long)]
+    #[structopt(short = "H", long, required=false, default_value="")]
     host: String,
 
     /// Rust module file to import, e.g. '../other.rs'.
-    #[structopt(short = "f", long, default_value="goosefile")]
-    goose_file: String,
+    //#[structopt(short = "f", long, default_value="goosefile")]
+    //goosefile: String,
 
     /// Number of concurrent Goose users.
     #[structopt(short, long, default_value="1")]
@@ -26,7 +29,7 @@ pub struct Configuration {
     hatch_rate: usize,
 
     /// Stop after the specified amount of time, e.g. (300s, 20m, 3h, 1h30m, etc.).
-    #[structopt(short = "t", long)]
+    #[structopt(short = "t", long, required=false, default_value="")]
     run_time: String,
 
     /// Prints stats in the console
@@ -46,21 +49,64 @@ pub struct Configuration {
     list: bool,
 
     /// Number of seconds to wait for a simulated user to complete any executing task before existing. Default is to terminate immediately.
-    #[structopt(short, long)]
+    #[structopt(short, long, required=false, default_value="0")]
     stop_timeout: usize,
 }
 
-// Attempt to locate a goosefile, either explicitly or by searching parent dirs.
-fn find_goose_file() -> Option<PathBuf> {
+// Locate goosefile dynamic library
+fn find_goosefile() -> Option<PathBuf> {
     // @TODO: emulate how Locust does this
     //  - allow override in env
-    //  - optionally append ".rs" ie "goosefile.rs"
     //  - search from current directory up
     //  - return None if no goosefile is found
-    Some(PathBuf::from("goosefile"))
+    Some(PathBuf::from("goosefile.rs"))
+}
+
+// Load goosefile dynamic library and extract geese
+fn load_goosefile(_goosefile: PathBuf) -> Option<Vec<String>> {
+    Some(vec!["@TODO".to_string()])
 }
 
 fn main() {
-    let _configuration = Configuration::from_args();
-    let _goose_file = find_goose_file();
+    let configuration = Configuration::from_args();
+
+    // @TODO: Logger
+
+    let goosefile = match find_goosefile() {
+        Some(g) => g,
+        None => {
+            eprintln!("Could not find any goosefile! Ensure file ends with '.rs' and see --help for availble options.");
+            std::process::exit(1);
+        }
+    };
+
+    if goosefile.file_name() == Some(OsStr::new("goose.rs")) {
+        eprintln!("The goosfile must not be named `goose.rs`. Please rename the file and try again.");
+        std::process::exit(1);
+    }
+
+    let geese = match load_goosefile(goosefile) {
+        Some(g) => g,
+        None => {
+            eprintln!("No geese found in the goosefile! Please create a test plan and try again.");
+            std::process::exit(1);
+        }
+    };
+
+    if configuration.list {
+        println!("Available Geese:");
+        for goose in geese {
+            println!(" - {}", goose);
+        }
+        std::process::exit(0);
+    }
+
+    let run_time: usize;
+    if configuration.run_time != "" {
+        run_time = util::parse_timespan(&configuration.run_time);
+    }
+    else {
+        run_time = 0;
+    }
+    println!("run_time = {}", run_time);
 }
---
+++ src/util.rs
@@ -0,0 +1,30 @@
+use std::str::FromStr;
+use regex::Regex;
+
+/// Parse a string representing a time span and return the number of seconds.
+/// Valid formats are: 20, 20s, 3m, 2h, 1h20m, 3h30m10s, etc.
+pub fn parse_timespan(time_str: &str) -> usize {
+    let time = match usize::from_str(time_str) {
+        // If an integer is passed in, assume it's seconds
+        Ok(t) => t,
+        // Otherwise use a regex to extract hours, minutes and seconds from string.
+        Err(_) => {
+            let re = Regex::new(r"((?P<hours>\d+?)h)?((?P<minutes>\d+?)m)?((?P<seconds>\d+?)s)?").unwrap();
+            let time_matches = re.captures(time_str).unwrap();
+            let hours = match time_matches.name("hours") {
+                Some(_) => usize::from_str(&time_matches["hours"]).unwrap(),
+                None => 0,
+            };
+            let minutes = match time_matches.name("minutes") {
+                Some(_) => usize::from_str(&time_matches["minutes"]).unwrap(),
+                None => 0,
+            };
+            let seconds = match time_matches.name("seconds") {
+                Some(_) => usize::from_str(&time_matches["seconds"]).unwrap(),
+                None => 0,
+            };
+            hours * 60 * 60 + minutes * 60 + seconds
+        }
+    };
+    time
+}
