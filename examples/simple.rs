4b12521256f24379add122226cbd5b8ecef49964
Jeremy Andrews <jeremy@tag1consulting.com>
2020-04-21 11:28:12 +0200

    Apache-2.0 license

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,4 +1,20 @@
 //! Simple Goose load test example. Duplicates the simple example on Locust project page.
+//! 
+//! ## License
+//! 
+//! Copyright 2020 Jeremy Andrews
+//! 
+//! Licensed under the Apache License, Version 2.0 (the "License");
+//! you may not use this file except in compliance with the License.
+//! You may obtain a copy of the License at
+//! 
+//! http://www.apache.org/licenses/LICENSE-2.0
+//! 
+//! Unless required by applicable law or agreed to in writing, software
+//! distributed under the License is distributed on an "AS IS" BASIS,
+//! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//! See the License for the specific language governing permissions and
+//! limitations under the License.
 
 use goose::GooseState;
 use goose::goose::{GooseTaskSet, GooseClient, GooseTask};
@@ -27,6 +43,8 @@ fn website_task_login(client: &mut GooseClient) {
 
 fn website_task_index(client: &mut GooseClient) {
     let _response = client.get("/");
+    //client.set_response_err("error text here");
+    //client.set_response_ok();
 }
 
 fn website_task_about(client: &mut GooseClient) {
