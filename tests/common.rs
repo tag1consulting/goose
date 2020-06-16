5049647c4abb51a02170242010d2e01f8045f7bc
Jeremy Andrews <jeremy@tag1consulting.com>
2020-06-16 10:36:13 +0200

    add no_sticky_follow to build_configuration

--- tests/common.rs
+++ tests/common.rs
@@ -16,6 +16,7 @@ pub fn build_configuration() -> GooseConfiguration {
         verbose: 0,
         log_level: 0,
         log_file: "goose.log".to_string(),
+        no_sticky_follow: false,
         manager: false,
         no_hash_check: false,
         expect_workers: 0,
