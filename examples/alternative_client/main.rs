0f5d7cd0917d47fe7d767035f4c47c08831db943
Jeremy Andrews <jeremy@tag1consulting.com>
2021-12-05 08:22:11 +0100

    add log in and authenticated users

--- examples/alternative_client/main.rs
+++ examples/alternative_client/main.rs
@@ -26,6 +26,7 @@ use goose::prelude::*;
 
 use isahc::prelude::*;
 use rand::Rng;
+use regex::Regex;
 
 #[tokio::main]
 async fn main() -> Result<(), GooseError> {
@@ -49,7 +50,6 @@ async fn main() -> Result<(), GooseError> {
                         .set_name("(Anon) user page"),
                 ),
         )
-        /*
         .register_taskset(
             taskset!("AuthBrowsingUser")
                 .set_weight(1)?
@@ -72,14 +72,15 @@ async fn main() -> Result<(), GooseError> {
                     task!(drupal_memcache_profile_page)
                         .set_weight(3)?
                         .set_name("(Auth) user page"),
-                )
-                .register_task(
-                    task!(drupal_memcache_post_comment)
-                        .set_weight(3)?
-                        .set_name("(Auth) comment form"),
                 ),
+            /*
+                    .register_task(
+                        task!(drupal_memcache_post_comment)
+                            .set_weight(3)?
+                            .set_name("(Auth) comment form"),
+                    ),
+            */
         )
-        */
         .execute()
         .await?
         .print();
@@ -249,6 +250,147 @@ async fn drupal_memcache_profile_page(user: &mut GooseUser) -> GooseTaskResult {
             user.send_request_metric_to_parent(request_metric.clone())?;
             return user.set_failure(
                 &format!("user page: no response from server: {}", e),
+                &mut request_metric,
+                None,
+                None,
+            );
+        }
+    }
+
+    Ok(())
+}
+
+/// Log in.
+async fn drupal_memcache_login(user: &mut GooseUser) -> GooseTaskResult {
+    let started = std::time::Instant::now();
+    let url = user.build_url("/user").unwrap();
+    let response = isahc::get_async(&url).await;
+
+    match response {
+        Ok(mut r) => {
+            // Copy the headers so we have them for logging if there are errors.
+            let headers = &r.headers().clone();
+            let status = r.status();
+            let mut request_metric = util::build_request_metric(
+                user,
+                GooseMethod::Get,
+                &url,
+                Some(headers),
+                "",
+                started,
+                status,
+            );
+            request_metric.name = "/user".to_string();
+            user.send_request_metric_to_parent(request_metric.clone())?;
+
+            match r.text().await {
+                Ok(html) => {
+                    let re = Regex::new(r#"name="form_build_id" value=['"](.*?)['"]"#).unwrap();
+                    let form_build_id = match re.captures(&html) {
+                        Some(f) => f,
+                        None => {
+                            // This will automatically get written to the error log if enabled, and will
+                            // be displayed to stdout if `-v` is enabled when running the load test.
+                            return user.set_failure(
+                                "login: no form_build_id on page: /user page",
+                                &mut request_metric,
+                                Some(headers),
+                                Some(&html),
+                            );
+                        }
+                    };
+
+                    // Log the user in.
+                    let uid: usize = rand::thread_rng().gen_range(3..5_002);
+                    let username = format!("user{}", uid);
+                    let params = format!(
+                        r#"{{
+                        "name": "{}",
+                        "pass": "12345",
+                        "form_build_id": "{}",
+                        "form_id": "user_login",
+                        "op": "Log+in",
+                    }}"#,
+                        username.as_str(),
+                        &form_build_id[1],
+                    );
+                    let started = std::time::Instant::now();
+                    let response = isahc::post_async(&url, params.as_str()).await;
+                    match response {
+                        Ok(mut r) => {
+                            // Copy the headers so we have them for logging if there are errors.
+                            let headers = &r.headers().clone();
+                            let status = r.status();
+                            let mut request_metric = util::build_request_metric(
+                                user,
+                                GooseMethod::Post,
+                                &url,
+                                Some(headers),
+                                "",
+                                started,
+                                status,
+                            );
+                            request_metric.name = "/user".to_string();
+                            r.consume().await.unwrap();
+                            user.send_request_metric_to_parent(request_metric)?;
+                        }
+                        Err(e) => {
+                            let mut request_metric = util::build_request_metric(
+                                user,
+                                GooseMethod::Post,
+                                &url,
+                                None,
+                                &e.to_string(),
+                                started,
+                                http::StatusCode::from_u16(500).unwrap(),
+                            );
+                            request_metric.name = "/user".to_string();
+                            user.send_request_metric_to_parent(request_metric.clone())?;
+                            return user.set_failure(
+                                &format!("user page: no response from server: {}", e),
+                                &mut request_metric,
+                                None,
+                                None,
+                            );
+                        }
+                    }
+                }
+                Err(e) => {
+                    let status = r.status();
+                    let mut request_metric = util::build_request_metric(
+                        user,
+                        GooseMethod::Get,
+                        &url,
+                        Some(headers),
+                        &e.to_string(),
+                        started,
+                        status,
+                    );
+                    request_metric.name = "/".to_string();
+                    user.send_request_metric_to_parent(request_metric.clone())?;
+                    return user.set_failure(
+                        &format!("login: failed to parse page: {}", e),
+                        &mut request_metric,
+                        Some(headers),
+                        None,
+                    );
+                }
+            }
+        }
+        Err(e) => {
+            let mut request_metric = util::build_request_metric(
+                user,
+                GooseMethod::Get,
+                &url,
+                None,
+                &e.to_string(),
+                started,
+                http::StatusCode::from_u16(500).unwrap(),
+            );
+            request_metric.name = "/user".to_string();
+            user.send_request_metric_to_parent(request_metric.clone())?;
+            return user.set_failure(
+                &format!("login: no response from server: {}", e),
                 &mut request_metric,
                 None,
                 None,
